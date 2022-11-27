dde27bb515a31c87471f845182ade1389aa093d0
Alex de la Iglesia <alexander.delaiglesia@gmail.com>
2022-11-27 11:18:26 -0500

    Moved things

--- approx-mwm/main.py
+++ approx-mwm/main.py
@@ -1,4 +1,32 @@
-if __name__ == "__main__":
-    graph = Graph()
-    matching = Matching(graph)
-    display(matching)
+from matching import matching
+from display import display
+
+line = input()
+n, m = line.split()
+n = int(n)
+m = int(m)
+
+vertices = {}
+edges = []
+
+for i in range(m):
+    edge = input()
+    a, b = edge.split()
+    
+    if a not in vertices:
+        vertices[a] = []
+    if b not in vertices:
+        vertices[b] = []
+    
+    edges.append([a, b])
+    vertices[a].append(b)
+    vertices[b].append(a)
+
+# print(vertices)
+
+matching = matching(vertices=vertices)
+
+print(f"largest matching was size {len(matching)}")
+print(matching)
+
+display(vertices, edges)
